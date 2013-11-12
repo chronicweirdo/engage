9c7cbe897b6ce0bae156c07a8bdfe496170fd173
Silviu <silviu.cacoveanu@gmail.com>
2013-11-12 16:51:37 +0100

    tutorial: large text field and actions

--- src/com/chronicweirdo/engage/MainActivity.java
+++ src/com/chronicweirdo/engage/MainActivity.java
@@ -4,6 +4,8 @@ import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.EditText;
 
@@ -16,12 +18,64 @@ public class MainActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
 	}
-
+	
 	@Override
 	public boolean onCreateOptionsMenu(Menu menu) {
-		// Inflate the menu; this adds items to the action bar if it is present.
-		getMenuInflater().inflate(R.menu.main, menu);
-		return true;
+	    // Inflate the menu items for use in the action bar
+	    MenuInflater inflater = getMenuInflater();
+	    inflater.inflate(R.menu.main, menu);
+	    return super.onCreateOptionsMenu(menu);
+	}
+	
+	@Override
+	public boolean onOptionsItemSelected(MenuItem item) {
+	    // Handle presses on the action bar items
+	    switch (item.getItemId()) {
+	        case R.id.action_search:
+	            openSearch();
+	            return true;
+	        case R.id.action_settings:
+	            openSettings();
+	            return true;
+	        case R.id.action_open:
+	        	openFile();
+	        	return true;
+	        case R.id.action_save:
+	        	saveFile();
+	        	return true;
+	        case R.id.action_close:
+	        	closeFile();
+	        	return true;
+	        case R.id.action_new:
+	        	newFile();
+	        	return true;
+	        default:
+	            return super.onOptionsItemSelected(item);
+	    }
+	}
+	
+	private void openFile() {
+		
+	}
+	
+	private void saveFile() {
+		
+	}
+	
+	private void closeFile() {
+		
+	}
+	
+	private void newFile() {
+		
+	}
+	
+	private void openSearch() {
+		
+	}
+	
+	private void openSettings() {
+		
 	}
 
 	/** Called when the user clicks the Send button */
